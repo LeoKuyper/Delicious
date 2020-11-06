8701e80e0b773b5b4308a27367b8de55a0ae08b8
Leo Kuyper <leokuyper@gmail.com>
2020-11-06 20:36:37 +0200

    Final

--- Delicious/Delicious/HomeViewController.swift
+++ Delicious/Delicious/HomeViewController.swift
@@ -7,42 +7,50 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeViewController: UIViewController{
     
-    @IBOutlet weak var recipeTable: UITableView!
-    
-    let recipeNames = ["Cake", "Soup", "Cupcake"]
-    let recipeDescription = ["Cake1", "Soup1", "Cupcake1"]
-    let recipeImage = ["food", "food", "food"]
 
+    
+    @IBOutlet weak var mainButton: UIButton!
+    
+    @IBOutlet weak var mainButton2: UIButton!
+    @IBOutlet weak var mainButton3: UIButton!
+    @IBOutlet weak var mainButton4: UIButton!
+    @IBOutlet weak var mainButton5: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.setHidesBackButton(true, animated: true)
+
+//        mainButton.layer.shadowColor = UIColor.black.cgColor
+//        mainButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+//        mainButton.layer.shadowRadius = 5
+//        mainButton.layer.shadowOpacity = 0.3
+//        
+//        mainButton2.layer.shadowColor = UIColor.black.cgColor
+//        mainButton2.layer.shadowOffset = CGSize(width: 3, height: 3)
+//        mainButton2.layer.shadowRadius = 5
+//        mainButton2.layer.shadowOpacity = 0.3
+//        
+//        mainButton3.layer.shadowColor = UIColor.black.cgColor
+//        mainButton3.layer.shadowOffset = CGSize(width: 3, height: 3)
+//        mainButton3.layer.shadowRadius = 5
+//        mainButton3.layer.shadowOpacity = 0.3
+//        
+//        mainButton4.layer.shadowColor = UIColor.black.cgColor
+//        mainButton4.layer.shadowOffset = CGSize(width: 3, height: 3)
+//        mainButton4.layer.shadowRadius = 5
+//        mainButton4.layer.shadowOpacity = 0.3
+//        
+//        mainButton5.layer.shadowColor = UIColor.black.cgColor
+//        mainButton5.layer.shadowOffset = CGSize(width: 3, height: 3)
+//        mainButton5.layer.shadowRadius = 5
+//        mainButton5.layer.shadowOpacity = 0.3
         
-        let nib = UINib(nibName: "TemplateTableCell", bundle: nil)
-        recipeTable.register(nib, forCellReuseIdentifier: "TemplateTableCell")
-        
-        self.recipeTable.rowHeight = 400
-        
-        recipeTable.delegate = self
-        recipeTable.dataSource = self
 
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipeNames.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = recipeTable.dequeueReusableCell(withIdentifier: "TemplateTableCell", for: indexPath) as! TemplateTableCell
-        
-        cell.recipeName?.text = recipeNames[indexPath.row]
-        cell.recipeDescription?.text = recipeDescription[indexPath.row]
-        cell.recipeImage?.image = UIImage(named: recipeImage[indexPath.row])
-               
-        return cell
-    }
 
 }
